e73954f7d402f921f3445fe534246da20f91292a
Mickey_Khuong <tranlekhuong2001@gmail.com>
2021-08-25 12:42:15 +0700

    first commit

---
+++ s.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+ int main()
+ {
+	 int a,b;
+	 cout<<"Enter two number";
+	 cin>>a>>b;
+	 cout<<"Sum of num is: "<<a+b;
+	 return 0;
+
+ }
